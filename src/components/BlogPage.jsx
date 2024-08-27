757ba0f5f3f4f84a83bb1ab967bd04ed917e3df3
Bhavesh <bhaveshrajput9682@gmail.com>
2024-08-27 17:04:07 +0530

    update blogpage

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -11,11 +11,11 @@ const posts = [
       datetime: '2020-03-16',
       category: { title: 'Marketing', href: '#' },
       author: {
-        name: 'Michael Foster',
+        name: 'Bhavesh Singh',
         role: 'Co-Founder / CTO',
         href: '#',
         imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          '',
       },
     },
     {
